a497291996a12f86079af5cf9f155881bb11ece0
Bowen <hwbrzzl@gmail.com>
2024-06-10 22:06:07 +0800

    upgrade v1.14.0

--- controller.go
+++ controller.go
@@ -1,11 +1,15 @@
 package gateway
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	contractshttp "github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/support/json"
+	"github.com/spf13/cast"
 )
 
 func Get(ctx contractshttp.Context) contractshttp.Response {
@@ -29,23 +33,47 @@ func Patch(ctx contractshttp.Context) contractshttp.Response {
 }
 
 func request(ctx contractshttp.Context, method string) contractshttp.Response {
-	var body io.Reader
-	if method != http.MethodGet {
-		body = ctx.Request().Origin().Body
-	}
-
+	var body *bytes.Buffer
 	fallback := FacadesConfig.Get("gateway.fallback").(func(ctx contractshttp.Context, err error) contractshttp.Response)
 	url := fmt.Sprintf("http://%s:%s%s", FacadesConfig.GetString("gateway.host"), FacadesConfig.GetString("gateway.port"), ctx.Request().Path())
+
+	if method == http.MethodGet {
+		if rawQuery := ctx.Request().Origin().URL.RawQuery; rawQuery != "" {
+			url += "?" + rawQuery
+		}
+	} else {
+		var err error
+		contentType := ctx.Request().Header("Content-Type")
+		if strings.Contains(contentType, "application/json") {
+			body, err = AddInjectForJson(ctx.Request().Origin())
+			if err != nil {
+				return fallback(ctx, err)
+			}
+		}
+		if strings.Contains(contentType, "application/x-www-form-urlencoded") {
+			body, err = AddInjectForForm(ctx.Request().Origin())
+			if err != nil {
+				return fallback(ctx, err)
+			}
+		}
+	}
+
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return fallback(ctx, err)
 	}
 
-	query := ctx.Request().Origin().URL.Query()
-	req.URL.RawQuery = query.Encode()
+	//query := ctx.Request().Origin().URL.Query()
+	//req.URL.RawQuery = query.Encode()
 	for key, header := range ctx.Request().Headers() {
-		req.Header.Set(key, ctx.Request().Header(header[0]))
+		if key == "Content-Length" {
+			req.Header.Set(key, cast.ToString(body.Len()))
+		} else {
+			req.Header.Set(key, header[0])
+		}
 	}
+	//req.Header = ctx.Request().Origin().Header.Clone()
+	//req.ContentLength = int64(body.Len())
 
 	gatewayResp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -65,4 +93,50 @@ func request(ctx contractshttp.Context, method string) contractshttp.Response {
 	}
 
 	return resp.Data(200, ctx.Request().Header("Content-Type", "application/json"), content)
+}
+
+func AddInjectForJson(request *http.Request) (*bytes.Buffer, error) {
+	injectData := request.URL.Query()
+	originBody := request.Body
+
+	body, err := io.ReadAll(originBody)
+	if err != nil {
+		return nil, err
+	}
+
+	var reqBody map[string]any
+	err = json.Unmarshal(body, &reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range injectData {
+		if _, exist := reqBody[key]; !exist {
+			reqBody[key] = value[0]
+		}
+	}
+
+	newBody, err := json.Marshal(reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBuffer(newBody), nil
+}
+
+func AddInjectForForm(request *http.Request) (*bytes.Buffer, error) {
+	injectData := request.URL.Query()
+	if request.PostForm == nil {
+		if err := request.ParseForm(); err != nil {
+			return nil, fmt.Errorf("parse form error: %v", err)
+		}
+	}
+
+	for key, value := range injectData {
+		if !request.PostForm.Has(key) {
+			request.PostForm.Set(key, value[0])
+		}
+	}
+
+	return bytes.NewBufferString(request.PostForm.Encode()), nil
 }
